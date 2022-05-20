1924062c671cbd1a02053758b71e475cc55c05c4
vidurshan <vidurshan1996>
2022-05-20 12:29:41 +0530

    intial commit

---
+++ src/main/java/com/stickynotes/stickynnotes/StickynnotesApplication.java
@@ -0,0 +1,13 @@
+package com.stickynotes.stickynnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickynnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickynnotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stickynotes/stickynnotes/StickynnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stickynotes.stickynnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickynnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
